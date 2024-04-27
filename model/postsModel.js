05f07caf8adbe661992ce49cee97e9723b78b613
divine <divineosuuweb@gmail.com>
2024-04-27 16:10:12 +0200

    Getting all posts made in a group and datas of the user who made the post

--- model/postsModel.js
+++ model/postsModel.js
@@ -5,6 +5,10 @@ class Posts{
         const [rows] = await dbQuery('INSERT INTO posts (post_content, group_id, user_id) VALUES (?, ?, ?)', [postContent, groupId, userId]);
         return rows;
     }
+    static async selectAllPostWithUser(groupId){
+        const [rows] = await dbQuery('SELECT * FROM  posts JOIN  users ON posts.user_id = users.user_id WHERE group_id = ?', [groupId]);
+        return rows;
+    }
 }
 
 export default Posts;
--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import Posts from '../model/postsModel.js';
 import verifyToken from "../middlewares/webtokenMiddleware.js";
 import postInsertion from '../middlewares/ValidatePostsInsertMiddleware.js';
+import getPosts from '../middlewares/GetPostMiddleware.js';
 
 
 const router = Router();
@@ -17,6 +18,21 @@ router.post('/', postInsertion, verifyToken, async (req, res) => {
         res.status(500).json({ error: 'Internal server error' });
     }
 });
+
+router.get('/:id', getPosts, verifyToken, async (req, res) => {
+    try {
+        const groupId = req.params.id;
+        const getPost = await Posts.selectAllPostWithUser(groupId);
+        if(!getPost.length > 0){
+            return res.status(404).json({ message: 'No post found' });
+        }
+        res.send(getPost); // Send only the count value
+    } catch (error) {
+        console.error('Error fetching posts:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
 
 
 export default router;
