537d08130d4ee913f91890e9973dfa4545e7364d
divine <divineosuuweb@gmail.com>
2024-06-01 14:20:43 +0200

    Adding an option in userAndGroupsModel.js to set the default value to false when user joins a group

--- model/UsersAndGroupsModel.js
+++ model/UsersAndGroupsModel.js
@@ -1,8 +1,9 @@
 import { dbQuery } from "../db/db.js";
 
 class UsersGroups {
-    static async addUserGroups(userId, groupId) {
-        const [rows] = await dbQuery('INSERT INTO users_actionsgroups(user_id, group_id) VALUES (?, ?)', [userId, groupId])
+    static async addUserGroups(userId, groupId, quitGroup = false) {
+        const [rows] = await dbQuery('INSERT INTO users_actionsgroups(user_id, group_id, quit_group) VALUES (?, ?, ?)', [userId, groupId, quitGroup])
+        return rows;
     }
     static async CheckUserAlreadyExistsInAGroup(userId, groupId) {
         const [rows] = await dbQuery('SELECT COUNT(*) AS count FROM users_actionsgroups WHERE user_id = ? AND group_id = ?', [userId, groupId])
